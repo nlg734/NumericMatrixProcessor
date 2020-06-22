a0efaaab31ab397c685c4fb65ed31e5e0bb890c5
nlg734 <npsm111@gmail.com>
2020-06-22 14:43:05 -0500

    stage 2 complete

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -11,12 +11,23 @@ def matrix_sum(rows, columns, A, B):
         result.append(row_totals)
     return result
 
+
+def scalar_mult(rows, columns, A, c):
+    result = []
+    for m in range(int(rows)):
+        row_totals = []
+        for n in range(int(columns)):
+            row_totals.append(c * int(A[m][n]))
+        result.append(row_totals)
+    return result
+
+
 rowA, columnA = input().split()
 matrix_A = []
 for _a in range(int(rowA)):
     matrix_A.append(input().split())
 
-rowB, columnB = input().split()
+'''rowB, columnB = input().split()
 matrix_B = []
 for _b in range(int(rowB)):
     matrix_B.append(input().split())
@@ -26,4 +37,10 @@ if rowA == rowB and columnA == columnB:
     for row in new:
         print(*row, sep=" ")
 else:
-    print("ERROR")
+    print("ERROR")'''
+
+mult = int(input())
+
+new = scalar_mult(rowA, columnA, matrix_A, mult)
+for row in new:
+    print(*row, sep=" ")
